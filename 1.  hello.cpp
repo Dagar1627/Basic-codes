8a17a6c22c22bafda1e185f3819d9689777d5a47
RITIK DAGAR <47457948+Ritik2703@users.noreply.github.com>
2020-10-03 09:40:01 +0530

    Add files via upload

---
+++ 1.  hello.cpp
@@ -0,0 +1,14 @@
+/*
+     hare krishna hare krishna krishna krishna hare hare
+     hare rama hare rama rama rama hare
+*/
+#include<bits/stdc++.h>
+#define ll long long int
+#define fast ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+using namespace std;
+int main()
+{
+   cout<<"hello cpp";
+   return 0;
+}
+
